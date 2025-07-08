1a6a03196e1e18fb5e64a00922ea033bb382b706
JP Codecraft <albion@gmail.com>
2025-07-08 10:14:52 -0300

    added twohanded prop

--- process_items.py
+++ process_items.py
@@ -9,7 +9,7 @@ KEEP_FIELDS = [
     "@baselootamount", "@famevalue", "@itemvalue", "@tier", "@weight", "@durability",
     "@enchantmentlevel", "craftingrequirements", "@destinycraftfamefactor", "famefillingmissions",
     "lootlist", "@maxfame", "enchantments", "@itempower", "@combatspecachievement",
-    "@maxqualitylevel", "@slottype", "harvest", "@foodcategory", "@nutrition",
+    "@maxqualitylevel", "@slottype", "harvest", "@foodcategory", "@nutrition", "@twohanded",
     "@placefame", "@kind", "@activefarmfocuscost", "@activefarmmaxcycles",
     "@activefarmactiondurationseconds", "@activefarmcyclelengthseconds", "@activefarmbonus",
     "grownitem", "consumption", "products", "@unlockedtoplace", "@unlockedtocraft", "craftingspelllist", "@transformation"
